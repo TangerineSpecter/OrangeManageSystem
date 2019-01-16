c9fb1d37fae4a95ea0be9c1e35a44d782767dca1
TangerineSpecter <993033472@qq.com>
2019-01-16 17:01:24 +0800

    fix(echarts):修复echarts初始化getAttribute为null问题

--- src/main/resources/static/js/omsJs/index/index.js
+++ src/main/resources/static/js/omsJs/index/index.js
@@ -1,15 +1,6 @@
 $(function() {
-	initSystemInfo();
 	initMenuBtn();
-	initStarECharts();
 })
-
-/**
- * 系统信息初始化
- */
-function initSystemInfo() {
-	$("#today-time").html(getToday());
-}
 
 /**
  * 加载内容
@@ -31,57 +22,4 @@ function initMenuBtn() {
 	$("#account-setting").click(function() {
 		loadUrlContent("accountSetting?id=1");
 	})
-}
-
-/**
- * 获取今天日期
- */
-function getToday() {
-	var myDate = new Date();
-	return myDate.getFullYear() + "-" + (myDate.getMonth() + 1) + "-"
-			+ myDate.getDate();
-}
-
-function initStarECharts() {
-	var myChart = echarts.init(document.getElementById('star-echarts'));
-	var option = {
-		tooltip : {
-			trigger : 'axis'
-		},
-		radar : [ {
-			indicator : [ {
-				text : '健康',
-				max : 100
-			}, {
-				text : '爱情',
-				max : 100
-			}, {
-				text : '财运',
-				max : 100
-			}, {
-				text : '工作',
-				max : 100
-			} ],
-			center : [ '50%', '50%' ],
-			radius : 50
-		} ],
-		series : [ {
-			type : 'radar',
-			tooltip : {
-				trigger : 'item'
-			},
-			itemStyle : {
-				normal : {
-					areaStyle : {
-						type : 'default'
-					}
-				}
-			},
-			data : [ {
-				value : [ 60, 73, 85, 40 ],
-				name : '星运图'
-			} ]
-		} ]
-	};
-	myChart.setOption(option);
 }

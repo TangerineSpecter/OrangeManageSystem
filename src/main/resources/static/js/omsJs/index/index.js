77c88fac28094fd3b51cc9374b4622053a9f230c
TangerineSpecter <993033472@qq.com>
2019-01-16 15:56:51 +0800

    add(index):增加首页雷达图

--- src/main/resources/static/js/omsJs/index/index.js
+++ src/main/resources/static/js/omsJs/index/index.js
@@ -1,6 +1,7 @@
 $(function() {
 	initSystemInfo();
 	initMenuBtn();
+	initStarECharts();
 })
 
 /**
@@ -26,7 +27,7 @@ function initMenuBtn() {
 		var url = $(this).attr("id");
 		loadUrlContent(url);
 	})
-	//帐号设置
+	// 帐号设置
 	$("#account-setting").click(function() {
 		loadUrlContent("accountSetting?id=1");
 	})
@@ -39,4 +40,48 @@ function getToday() {
 	var myDate = new Date();
 	return myDate.getFullYear() + "-" + (myDate.getMonth() + 1) + "-"
 			+ myDate.getDate();
+}
+
+function initStarECharts() {
+	var myChart = echarts.init(document.getElementById('star-echarts'));
+	var option = {
+		tooltip : {
+			trigger : 'axis'
+		},
+		radar : [ {
+			indicator : [ {
+				text : '健康',
+				max : 100
+			}, {
+				text : '爱情',
+				max : 100
+			}, {
+				text : '财运',
+				max : 100
+			}, {
+				text : '工作',
+				max : 100
+			} ],
+			center : [ '50%', '50%' ],
+			radius : 50
+		} ],
+		series : [ {
+			type : 'radar',
+			tooltip : {
+				trigger : 'item'
+			},
+			itemStyle : {
+				normal : {
+					areaStyle : {
+						type : 'default'
+					}
+				}
+			},
+			data : [ {
+				value : [ 60, 73, 85, 40 ],
+				name : '星运图'
+			} ]
+		} ]
+	};
+	myChart.setOption(option);
 }

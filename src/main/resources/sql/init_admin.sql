106fcc0b759bf4b62bc24aa00b56981096bb4bf4
TangerineSpecter <993033472@qq.com>
2019-01-26 23:46:10 +0800

    add(*):新增数据初始化

---
+++ src/main/resources/sql/init_admin.sql
@@ -0,0 +1,2 @@
+/* 系统用户数据初始化 */
+INSERT INTO `oms`.`system_user` (`id`, `username`, `password`, `nick_name`, `avatar`, `sex`, `phone_number`, `email`, `city`, `birthday`, `brief`, `create_time`, `update_time`, `admin`, `is_del`) VALUES ('1', 'admin', 'f37187234b2f5ad61cca6c93083c6ffa', '丢失的猫咪', NULL, '0', NULL, '993033472@qq.com', 'shenzhen', '2019-01-13', '哈哈', '2019-01-04 23:01:08', NULL, '1', '0');

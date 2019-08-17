1b40eea98339a82d4f6a83adf96c1feebbef8c16
TangerineSpecter <993033472@qq.com>
2019-08-18 05:21:20 +0800

    fix(*)：处理错误

--- src/main/java/com/tangerineSpecter/oms/system/service/system/SystemUserService.java
+++ src/main/java/com/tangerineSpecter/oms/system/service/system/SystemUserService.java
@@ -18,11 +18,11 @@ import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 
+import javax.annotation.Resource;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
@@ -30,9 +30,9 @@ import java.util.List;
 @Service
 public class SystemUserService {
 
-    @Autowired
+    @Resource
     private SystemUserMapper systemUserMapper;
-    @Autowired
+    @Resource
     private PageResultService pageResultService;
 
     /**
@@ -43,7 +43,7 @@ public class SystemUserService {
     public ServiceResult verifyLogin(AccountsInfo model) {
         String md5Pwd;
         try {
-            md5Pwd = MD5Utils.getMD5Pwd(model.getPassword(), CommonConstant.SALT);
+            md5Pwd = MD5Utils.getMd5Pwd(model.getPassword(), CommonConstant.SALT);
             UsernamePasswordToken token = new UsernamePasswordToken(model.getUsername(), md5Pwd);
             Subject subject = SecurityUtils.getSubject();
             subject.login(token);

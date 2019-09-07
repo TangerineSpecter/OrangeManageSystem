ae8e946b2f59d6a4fa104d2aed717cbbfb27a7a8
TangerineSpecter <993033472@qq.com>
2019-09-08 01:37:37 +0800

    fix:补充二维码访问路径

--- src/main/java/com/tangerineSpecter/oms/system/service/tools/QrCodeToolService.java
+++ src/main/java/com/tangerineSpecter/oms/system/service/tools/QrCodeToolService.java
@@ -7,6 +7,7 @@ import com.tangerinespecter.oms.system.domain.vo.tools.QrCodeInfoVo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.validation.Valid;
 import java.io.File;
 
 @Service
@@ -14,16 +15,23 @@ public class QrCodeToolService {
 
     @Value("${qrcode.path}")
     private String QrCodePath;
+    @Value("${qrcode.view.path}")
+    private String QrViewPath;
+    /**
+     * 二维码文件名
+     */
+    private String QrFileName = "/qrcode.jpg";
 
     public ServiceResult createQrCode(QrCodeInfoVo vo) {
         String uuid = IdUtil.randomUUID();
-        String qrPath = QrCodePath + uuid + "/qrcode.jpg";
+        String qrPath = QrCodePath + uuid + QrFileName;
         File dir = new File(qrPath);
         if (!dir.exists()) {
             dir.getParentFile().mkdirs();
         }
         QrCodeUtil.generate(vo.getContent(), vo.getWidth(), vo.getHeight(), new File(qrPath));
-        return ServiceResult.success(qrPath);
+        String viewPath = QrViewPath + uuid + QrFileName;
+        return ServiceResult.success(viewPath);
     }
 
 }

ed6bcb9ad354915b234f4c9970c629f1c451fbcc
TangerineSpecter <993033472@qq.com>
2021-08-24 13:31:58 +0800

    refact：恢复小工具页面

--- src/test/java/com/tangerineSpecter/oms/OmsApplicationTests.java
+++ src/test/java/com/tangerineSpecter/oms/OmsApplicationTests.java
@@ -1,25 +1,21 @@
 package com.tangerinespecter.oms;
 
-import com.tangerinespecter.oms.system.service.dump.DumpDataService;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OmsApplicationTests {
 
-    @Resource
-    private DumpDataService dumpDataService;
-
-    @Test
-    public void contextLoads() {
-        dumpDataService.dumpAdPlanTable(String.format("%s%s", DumpDataService.DATA_ROOT_DIR,
-                DumpDataService.DATA_CONSTELLATION));
-    }
+//    @Resource
+//    private DumpDataService dumpDataService;
+//
+//    @Test
+//    public void contextLoads() {
+//        dumpDataService.dumpAdPlanTable(String.format("%s%s", DumpDataService.DATA_ROOT_DIR,
+//                DumpDataService.DATA_CONSTELLATION));
+//    }
 
 }
 
--- src/test/java/com/tangerineSpecter/oms/TestRedisTemple.java
+++ src/test/java/com/tangerineSpecter/oms/TestRedisTemple.java
@@ -1,24 +1,20 @@
 package com.tangerinespecter.oms;
 
-import com.tangerinespecter.oms.system.service.helper.RedisHelper;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestRedisTemple {
 
-    @Resource
-    private RedisHelper redisHelper;
-
-    @Test
-    public void testRedis() {
-        //redisHelper.set("test1", "hello world");
-        //Object test1 = redisHelper.get("test1");
-        //System.out.println(test1.toString());
-    }
+//    @Resource
+//    private RedisHelper redisHelper;
+//
+//    @Test
+//    public void testRedis() {
+//        //redisHelper.set("test1", "hello world");
+//        //Object test1 = redisHelper.get("test1");
+//        //System.out.println(test1.toString());
+//    }
 }

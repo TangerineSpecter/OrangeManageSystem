4ff73ddf25d2c9b4e08b351eef5445df06dce314
TangerineSpecter <993033472@qq.com>
2021-09-18 00:45:10 +0800

    fix：修复菜单管理页面

--- src/main/java/com/tangerineSpecter/oms/system/controller/system/MenuSettingController.java
+++ src/main/java/com/tangerineSpecter/oms/system/controller/system/MenuSettingController.java
@@ -9,11 +9,12 @@ import com.tangerinespecter.oms.system.domain.vo.system.SystemMenuInfoVo;
 import com.tangerinespecter.oms.system.service.page.PageResultService;
 import com.tangerinespecter.oms.system.service.system.IMenuSettingService;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -27,112 +28,96 @@ import java.util.List;
  * @author TangerineSpecter
  * @date 2019年09月01日18:40:56
  */
-@Controller
+@RestController
 @RequestMapping("/system/menu")
 public class MenuSettingController {
 
-    @Resource
-    private IMenuSettingService menuSettingService;
-    @Resource
-    private PageResultService pageResultService;
+	@Resource
+	private IMenuSettingService menuSettingService;
+	@Resource
+	private PageResultService pageResultService;
 
-    /**
-     * 菜单管理
-     */
-    @ResponseBody
-    @RequiresPermissions("system:menu:page")
-    @RequestMapping(value = "/page", produces = "text/html;charset=UTF-8")
-    public String pageInfo(HttpServletRequest request, HttpServletResponse response, Model model) {
-        return pageResultService.getPageHtmlContent(request, response, model, PageModelKey.getSystemMenuPageKey, "layout/menuSetting");
-    }
+	/**
+	 * 菜单管理
+	 */
+	@ResponseBody
+	@RequiresPermissions("system:menu:page")
+	@RequestMapping(value = "/page", produces = "text/html;charset=UTF-8")
+	public String pageInfo(HttpServletRequest request, HttpServletResponse response, Model model) {
+		return pageResultService.getPageHtmlContent(request, response, model, PageModelKey.getSystemMenuPageKey, "layout/menuSetting");
+	}
 
-    /**
-     * 添加页面
-     */
-    @RequestMapping("/addPage")
-    public String addMenuPage(Model model) {
-        model.addAttribute("menuList", menuSettingService.listInfo());
-        return "system/addEditMenuSetting";
-    }
+	/**
+	 * 添加页面
+	 */
+	@RequestMapping("/addPage")
+	public ModelAndView addMenuPage(Model model) {
+		model.addAttribute("menuList", menuSettingService.listInfo());
+		return ServiceResult.jumpPage("system/addEditMenuSetting");
+	}
 
-    /**
-     * 菜单列表
-     *
-     * @return
-     */
-    @ResponseBody
-    @RequestMapping("/list")
-    public ServiceResult<Object> listInfo() {
-        return menuSettingService.listInfo();
-    }
+	/**
+	 * 菜单列表
+	 */
+	@RequestMapping("/list")
+	public ServiceResult<Object> listInfo() {
+		return menuSettingService.listInfo();
+	}
 
-    /**
-     * 添加菜单
-     *
-     * @return
-     */
-    @ResponseBody
-    @RequestMapping("/insert")
-    @LoggerInfo(value = "添加菜单", event = LogOperation.EVENT_ADD)
-    public ServiceResult insertInfo(@Valid() SystemMenuInfoVo vo) {
-        return menuSettingService.insertInfo(vo);
-    }
+	/**
+	 * 添加菜单
+	 */
+	@RequestMapping("/insert")
+	@LoggerInfo(value = "添加菜单", event = LogOperation.EVENT_ADD)
+	public ServiceResult insertInfo(@Valid() SystemMenuInfoVo vo) {
+		return menuSettingService.insertInfo(vo);
+	}
 
-    /**
-     * 编辑菜单
-     *
-     * @return
-     */
-    @ResponseBody
-    @RequestMapping("/update")
-    @LoggerInfo(value = "编辑菜单", event = LogOperation.EVENT_UPDATE)
-    public ServiceResult updateInfo(@Valid() SystemMenuInfoVo vo) {
-        return menuSettingService.updateInfo(vo);
-    }
+	/**
+	 * 编辑菜单
+	 */
+	@RequestMapping("/update")
+	@LoggerInfo(value = "编辑菜单", event = LogOperation.EVENT_UPDATE)
+	public ServiceResult updateInfo(@Valid() SystemMenuInfoVo vo) {
+		return menuSettingService.updateInfo(vo);
+	}
 
-    /**
-     * 菜单信息
-     *
-     * @return
-     */
-    @ResponseBody
-    @RequestMapping("/info")
-    public ServiceResult detailInfo(@RequestParam("id") Long id) {
-        return menuSettingService.detailInfo(id);
-    }
+	/**
+	 * 菜单信息
+	 */
+	@RequestMapping("/info")
+	public ServiceResult detailInfo(@RequestParam("id") Long id) {
+		return menuSettingService.detailInfo(id);
+	}
 
-    /**
-     * 删除菜单
-     *
-     * @param id 菜单ID
-     * @return
-     */
-    @ResponseBody
-    @RequestMapping("/delete")
-    @LoggerInfo(value = "删除菜单", event = LogOperation.EVENT_DELETE)
-    public ServiceResult deleteInfo(@RequestParam("id") Long id) {
-        return menuSettingService.deleteInfo(id);
-    }
+	/**
+	 * 删除菜单
+	 *
+	 * @param id 菜单ID
+	 */
+	@RequestMapping("/delete")
+	@LoggerInfo(value = "删除菜单", event = LogOperation.EVENT_DELETE)
+	public ServiceResult deleteInfo(@RequestParam("id") Long id) {
+		return menuSettingService.deleteInfo(id);
+	}
 
-    /**
-     * 置顶菜单
-     *
-     * @param id 菜单ID
-     * @return
-     */
-    @ResponseBody
-    @RequestMapping("/top")
-    @LoggerInfo(value = "置顶菜单", event = LogOperation.EVENT_UPDATE)
-    public ServiceResult topInfo(@RequestParam("id") Long id) {
-        return menuSettingService.topInfo(id);
-    }
+	/**
+	 * 置顶菜单
+	 *
+	 * @param id 菜单ID
+	 */
+	@RequestMapping("/top")
+	@LoggerInfo(value = "置顶菜单", event = LogOperation.EVENT_UPDATE)
+	public ServiceResult topInfo(@RequestParam("id") Long id) {
+		return menuSettingService.topInfo(id);
+	}
 
-    /**
-     * 初始化菜单code
-     *
-     * @return
-     */
-    public List<SystemMenu> initMenuCode() {
-        return menuSettingService.initMenuCode();
-    }
+	/**
+	 * 初始化菜单code
+	 *
+	 * @return
+	 */
+	public List<SystemMenu> initMenuCode() {
+		return menuSettingService.initMenuCode();
+	}
 }

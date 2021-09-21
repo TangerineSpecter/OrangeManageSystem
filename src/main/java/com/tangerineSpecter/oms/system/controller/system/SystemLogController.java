75dd92671762d34dab52e514860febac33c22729
TangerineSpecter <993033472@qq.com>
2021-09-21 23:35:50 +0800

    fix：修复系统设置相关列表页面

--- src/main/java/com/tangerineSpecter/oms/system/controller/system/SystemLogController.java
+++ src/main/java/com/tangerineSpecter/oms/system/controller/system/SystemLogController.java
@@ -4,10 +4,9 @@ import com.tangerinespecter.oms.common.query.SystemLogQueryObject;
 import com.tangerinespecter.oms.common.result.ServiceResult;
 import com.tangerinespecter.oms.system.service.system.ISystemLogService;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 
@@ -29,8 +28,8 @@ public class SystemLogController {
 	 */
 	@RequestMapping("/page")
 	@RequiresPermissions("system:log:page")
-	public String pageInfo() {
-		return "system/logger";
+	public ModelAndView pageInfo() {
+		return ServiceResult.jumpPage("system/logger");
 	}
 
 	/**

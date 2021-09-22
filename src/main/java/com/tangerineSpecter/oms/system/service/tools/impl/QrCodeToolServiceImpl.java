ada4df7d8a6a661483f8814a08b036d02662d291
TangerineSpecter <993033472@qq.com>
2021-09-22 21:39:44 +0800

    refactor：调整配置文件移除多余配置

--- src/main/java/com/tangerineSpecter/oms/system/service/tools/impl/QrCodeToolServiceImpl.java
+++ src/main/java/com/tangerineSpecter/oms/system/service/tools/impl/QrCodeToolServiceImpl.java
@@ -4,20 +4,10 @@ import cn.hutool.extra.qrcode.QrCodeUtil;
 import com.tangerinespecter.oms.common.result.ServiceResult;
 import com.tangerinespecter.oms.system.domain.vo.tools.QrCodeInfoVo;
 import com.tangerinespecter.oms.system.service.tools.IQrCodeToolService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QrCodeToolServiceImpl implements IQrCodeToolService {
-
-	@Value("${qrcode.path}")
-	private String QrCodePath;
-	@Value("${qrcode.view.path}")
-	private String QrViewPath;
-	/**
-	 * 二维码文件名
-	 */
-	private String QrFileName = "/qrcode.jpg";
 
 	@Override
 	public ServiceResult createQrCode(QrCodeInfoVo vo) {

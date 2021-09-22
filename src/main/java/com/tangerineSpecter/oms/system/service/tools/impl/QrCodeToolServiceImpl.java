938e1bcad512071e12f973798e0534ef2a1d5162
TangerineSpecter <993033472@qq.com>
2021-09-22 13:32:20 +0800

    fix：修复二维码生成，二维码剔除生成文件的形式，调整为base64解析

--- src/main/java/com/tangerineSpecter/oms/system/service/tools/impl/QrCodeToolServiceImpl.java
+++ src/main/java/com/tangerineSpecter/oms/system/service/tools/impl/QrCodeToolServiceImpl.java
@@ -1,7 +1,5 @@
 package com.tangerinespecter.oms.system.service.tools.impl;
 
-import cn.hutool.core.date.DateUtil;
-import cn.hutool.core.util.IdUtil;
 import cn.hutool.extra.qrcode.QrCodeUtil;
 import com.tangerinespecter.oms.common.result.ServiceResult;
 import com.tangerinespecter.oms.system.domain.vo.tools.QrCodeInfoVo;
@@ -9,32 +7,21 @@ import com.tangerinespecter.oms.system.service.tools.IQrCodeToolService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-
 @Service
 public class QrCodeToolServiceImpl implements IQrCodeToolService {
 
-    @Value("${qrcode.path}")
-    private String QrCodePath;
-    @Value("${qrcode.view.path}")
-    private String QrViewPath;
-    /**
-     * 二维码文件名
-     */
-    private String QrFileName = "/qrcode.jpg";
+	@Value("${qrcode.path}")
+	private String QrCodePath;
+	@Value("${qrcode.view.path}")
+	private String QrViewPath;
+	/**
+	 * 二维码文件名
+	 */
+	private String QrFileName = "/qrcode.jpg";
 
-    @Override
-    public ServiceResult createQrCode(QrCodeInfoVo vo) {
-        String uuid = IdUtil.randomUUID();
-        String qrPath = DateUtil.today() + "/" + uuid + QrFileName;
-        String savePath = QrCodePath + qrPath;
-        File dir = new File(savePath);
-        if (!dir.exists()) {
-            dir.getParentFile().mkdirs();
-        }
-        QrCodeUtil.generate(vo.getContent(), vo.getWidth(), vo.getHeight(), new File(savePath));
-        String viewPath = QrViewPath + qrPath;
-        return ServiceResult.success(viewPath);
-    }
+	@Override
+	public ServiceResult createQrCode(QrCodeInfoVo vo) {
+		return ServiceResult.success(QrCodeUtil.generatePng(vo.getContent(), vo.getWidth(), vo.getHeight()));
+	}
 
 }

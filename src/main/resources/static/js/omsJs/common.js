29d2a28f2ebb9888ceef109947ff3f7cb616c7ae
TangerineSpecter <993033472@qq.com>
2022-01-02 17:55:04 +0800

    refactor：抽离公共搜索方法

--- src/main/resources/static/js/omsJs/common.js
+++ src/main/resources/static/js/omsJs/common.js
@@ -1,16 +1,31 @@
 //请求url地址
 var url = '';
 
-// 提示框
-function promptModal(content) {
-	$("#prompt-modal-content").html(content);
-	$("#prompt-modal").modal();
-}
-
 // 去除最后一位字符串
 function removeLastOne(str) {
-	if(str == '' || str == undefined) {
-		return '';
-	}
-	return str.substring(0, str.length - 1);
+    if (str == '' || str == undefined) {
+        return '';
+    }
+    return str.substring(0, str.length - 1);
 }
+
+//表单搜索
+layui.use(['form', 'table'], function () {
+    var form = layui.form,
+        table = layui.table;
+
+    form.on('submit(data-search-btn)', function (data) {
+        table.reload('currentTableId', {
+            where: data.field
+        });
+        return false;
+    });
+
+    form.on('submit(data-reset-btn)', function () {
+        table.reload('currentTableId', {
+            where: ""
+        });
+        form.reset();
+        return false;
+    });
+});
